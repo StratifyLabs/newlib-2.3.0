1408c50fe82a2111af18c3969620e893a96edf1d
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2017-08-12 22:23:14 -0600

    Added flag to show if device is char device

--- newlib/libc/include/sys/_default_fcntl.h
+++ newlib/libc/include/sys/_default_fcntl.h
@@ -9,20 +9,23 @@ extern "C" {
 #define	_FOPEN		(-1)	/* from sys/file.h, kernel use only */
 #define	_FREAD		0x0001	/* read enabled */
 #define	_FWRITE		0x0002	/* write enabled */
-#define	_FAPPEND	0x0008	/* append (writes guaranteed at the end) */
+#define	_FAPPEND		0x0008	/* append (writes guaranteed at the end) */
 #define	_FMARK		0x0010	/* internal; mark during gc() */
 #define	_FDEFER		0x0020	/* internal; defer for next gc pass */
 #define	_FASYNC		0x0040	/* signal pgrp when data ready */
-#define	_FSHLOCK	0x0080	/* BSD flock() shared lock present */
-#define	_FEXLOCK	0x0100	/* BSD flock() exclusive lock present */
+#define	_FSHLOCK		0x0080	/* BSD flock() shared lock present */
+#define	_FEXLOCK		0x0100	/* BSD flock() exclusive lock present */
 #define	_FCREAT		0x0200	/* open with file create */
 #define	_FTRUNC		0x0400	/* open with truncation */
 #define	_FEXCL		0x0800	/* error on open if file exists */
 #define	_FNBIO		0x1000	/* non blocking I/O (sys5 style) */
 #define	_FSYNC		0x2000	/* do all writes synchronously */
 #define	_FNONBLOCK	0x4000	/* non blocking I/O (POSIX style) */
-#define	_FNDELAY	_FNONBLOCK	/* non blocking I/O (4.2 style) */
-#define	_FNOCTTY	0x8000	/* don't assign a ctty on this open */
+#define	_FNDELAY		_FNONBLOCK	/* non blocking I/O (4.2 style) */
+#define	_FNOCTTY		0x8000	/* don't assign a ctty on this open */
+#if defined(__StratifyOS__)
+#define _FCHAR		0x10000  //is a character device -- don't advance loc on read/write
+#endif
 
 #define	O_ACCMODE	(O_RDONLY|O_WRONLY|O_RDWR)
 
@@ -43,6 +46,11 @@ extern "C" {
 /*	O_NDELAY	_FNBIO 		set in include/fcntl.h */
 #define	O_NONBLOCK	_FNONBLOCK
 #define	O_NOCTTY	_FNOCTTY
+
+#if defined(__StratifyOS__)
+#define O_CHAR		_FCHAR
+#endif
+
 /* For machines which care - */
 #if defined (__CYGWIN__)
 #define _FBINARY        0x10000
